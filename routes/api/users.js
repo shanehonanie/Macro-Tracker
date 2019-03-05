0fd9c8ff82898db35d5a12d8eff701590c4dcc85
Shane Honanie <shaneh425@gmail.com>
2019-03-05 14:53:23 -0800

    Passport JWT Auth Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const keys = require('../../config/keys');
 
@@ -81,5 +82,20 @@ router.post('/login', (req, res) => {
 		});
 	});
 });
+
+// @route GET api/users/current
+// @desc Return the current user
+// @access Private
+router.get(
+	'/current',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		res.json({
+			id: req.user.id,
+			name: req.user.name,
+			email: req.user.email
+		});
+	}
+);
 
 module.exports = router;
