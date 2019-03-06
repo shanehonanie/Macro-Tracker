439bb9d233e2b16c9d0b7d9a15b2a965d9bc832a
Shane Honanie <shaneh425@gmail.com>
2019-03-06 12:43:08 -0800

    GET api/profile/all

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,6 +32,25 @@ router.get('/handle/:handle', (req, res) => {
 		.catch(err => res.status(404).json(err));
 });
 
+// @route GET api/profile/all
+// @desc Get all profiles
+// @access Public
+router.get('/all', (req, res) => {
+	const errors = {};
+
+	Profile.find()
+		//.populate()
+		.then(profiles => {
+			if (!profiles) {
+				errors.noProfile = 'There are no profiles';
+				return res.status(404).json(errors);
+			}
+
+			res.json(profiles);
+		})
+		.catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+});
+
 // @route GET api/profile/user/:user_id
 // @desc Get profile by user ID
 // @access Public
