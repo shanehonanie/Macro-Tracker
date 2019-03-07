48780f18db4911829f18b1116332aa5d59ec421c
Shane Honanie <shaneh425@gmail.com>
2019-03-07 13:32:26 -0800

    GET api/goals

--- routes/api/goals.js
+++ routes/api/goals.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Validation
+const validatorGoalInput = require('../../validation/goal');
+
+// Load Food & User Models
+const Goal = require('../../models/Goal');
+//const User = require('../../models/User');
 
 // @route GET api/goals/test
 // @desc Tests post route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Goals Works' }));
+
+// @route GET api/goals
+// @desc Tests post route
+// @access Public
+router.get(
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		const errors = {};
+
+		Goal.findOne({ user: req.user.id })
+			.then(goal => {
+				if (!goal) {
+					errors.noProfile = 'Profile not found for this user';
+					return res.status(404).json(errors);
+				}
+				res.json(profile);
+			})
+			.catch(err => res.status(404).json(err));
+	}
+);
 
 module.exports = router;
