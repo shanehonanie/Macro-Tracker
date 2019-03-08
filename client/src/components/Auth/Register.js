67ebe2f07d61b3021c6ca52731a0adbb6f30fd0d
Shane Honanie <shaneh425@gmail.com>
2019-03-07 23:26:22 -0800

    Register Error Handling & Display Messages

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
 
 export class Register extends Component {
 	// constructor() {
@@ -31,10 +32,12 @@ export class Register extends Component {
 		axios
 			.post('api/users/register', newUser)
 			.then(res => console.log(res.data))
-			.catch(err => console.log(err.response.data));
+			.catch(err => this.setState({ errors: err.response.data }));
 	};
 
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div className='register'>
 				<div className='container'>
@@ -44,46 +47,66 @@ export class Register extends Component {
 							<p className='lead text-center'>
 								Create your MacroTracker account
 							</p>
-							<form onSubmit={this.onSubmitHandler}>
+							<form noValidate onSubmit={this.onSubmitHandler}>
 								<div className='form-group'>
 									<input
 										type='text'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.name
+										})}
 										placeholder='Name'
 										name='name'
 										value={this.state.name}
 										onChange={this.onChangeHandler}
 									/>
+									{errors.name && (
+										<div className='invalid-feedback'>{errors.name}</div>
+									)}
 								</div>
 								<div className='form-group'>
 									<input
 										type='email'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
 										placeholder='Email Address'
 										name='email'
 										value={this.state.email}
 										onChange={this.onChangeHandler}
 									/>
+									{errors.email && (
+										<div className='invalid-feedback'>{errors.email}</div>
+									)}
 								</div>
 								<div className='form-group'>
 									<input
 										type='password'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
 										placeholder='Password'
 										name='password'
 										value={this.state.password}
 										onChange={this.onChangeHandler}
 									/>
+									{errors.password && (
+										<div className='invalid-feedback'>{errors.password}</div>
+									)}
 								</div>
 								<div className='form-group'>
 									<input
 										type='password'
-										className='form-control form-control-lg'
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password2
+										})}
 										placeholder='Confirm Password'
 										name='password2'
 										value={this.state.password2}
 										onChange={this.onChangeHandler}
 									/>
+									{errors.password2 && (
+										<div className='invalid-feedback'>{errors.password2}</div>
+									)}
 								</div>
 								<input type='submit' className='btn btn-info btn-block mt-4' />
 							</form>
