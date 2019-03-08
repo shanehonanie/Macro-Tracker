df56ef20b01033b9e6a1345e9e8f60b504921380
Shane Honanie <shaneh425@gmail.com>
2019-03-07 18:12:53 -0800

    Routing Basic Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Landing from './components/Layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
-				<Navbar />
-				<Landing />
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<div className='App'>
+					<Navbar />
+					<Route exact path='/' component={Landing} />
+					<div className='container'>
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/login' component={Login} />
+					</div>
+					<Footer />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
