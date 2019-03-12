3fb4ae6c6c38568cccba5fe9bc1e25ba310bf56f
Shane Honanie <shaneh425@gmail.com>
2019-03-12 15:46:26 -0700

    Change to functional component

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export class Landing extends Component {
-	render() {
-		return (
-			<div className='landing'>
-				<div className='dark-overlay landing-inner text-light'>
-					<div className='container'>
-						<div className='row'>
-							<div className='col-md-12 text-center'>
-								<h1 className='display-3 mb-4'>Macro Tracker</h1>
-								<p className='lead'>
-									{' '}
-									Track daily macros to keep you on track for success
-								</p>
-								<hr />
-								<Link to='/register' className='btn btn-lg btn-info mr-2'>
-									Sign Up
-								</Link>
-								<Link to='/login' className='btn btn-lg btn-light'>
-									Login
-								</Link>
-							</div>
+const landing = () => {
+	return (
+		<div className='landing'>
+			<div className='dark-overlay landing-inner text-light'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-md-12 text-center'>
+							<h1 className='display-3 mb-4'>Macro Tracker</h1>
+							<p className='lead'>
+								{' '}
+								Track daily macros to keep you on track for success
+							</p>
+							<hr />
+							<Link to='/register' className='btn btn-lg btn-info mr-2'>
+								Sign Up
+							</Link>
+							<Link to='/login' className='btn btn-lg btn-light'>
+								Login
+							</Link>
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
-export default Landing;
+export default landing;
