32a176cd4a3e5e337d5bc2f7aaf744b6e9aacb35
Shane Honanie <shaneh425@gmail.com>
2019-03-14 14:13:37 -0700

    Food DB ListGroup

--- client/src/containers/Food/AddTo/Breakfest.js
+++ client/src/containers/Food/AddTo/Breakfest.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from '../../../store/actions/index';
 
 export class Breakfest extends Component {
+	state = {
+		selectedItem: -1
+	};
+
+	componentDidMount() {
+		this.props.onGetFoodsDatabase();
+	}
+
 	render() {
-		return (
-			<div>
-				<h1>Breakfest</h1>
-			</div>
-		);
+		let data = null;
+		let displayFoodList = null;
+		let displayFoodItem = null;
+
+		if (!this.props.loading && this.props.foodsDatabase) {
+			console.log('this.props.foodsDatabase', this.props.foodsDatabase);
+
+			data = this.props.foodsDatabase.map((food, index) => {
+				console.log(`Food: ${food.name} -- Index: ${index}`);
+				return (
+					<button
+						type='button'
+						key={index}
+						className='list-group-item list-group-item-action'
+					>
+						{food.name}
+					</button>
+				);
+			});
+
+			displayFoodList = (
+				<div className='list-group offset-lg-1 col-lg-4'>{data}</div>
+			);
+		}
+		return <div className='container'>{displayFoodList}</div>;
 	}
 }
 
-export default Breakfest;
+const mapStateToProps = state => {
+	return {
+		error: state.profile.error,
+		token: state.auth.token,
+		foodsDatabase: state.food.foods,
+		loading: state.food.loading
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onCreateFoodsHistory: (foodsHistoryData, token) =>
+			dispatch(actions.addFoodsHistory(foodsHistoryData, token)),
+		onGetFoodsDatabase: () => dispatch(actions.fetchFoods())
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Breakfest);
