21eee72d41c2fce938d6e040979606e9fb1672ba
Shane Honanie <shaneh425@gmail.com>
2019-03-15 10:26:23 -0700

    Refactor into one file

--- client/src/components/MealTable/MealTable.js
+++ client/src/components/MealTable/MealTable.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import MealTableRow from './MealTableRow';
 import { Link } from 'react-router-dom';
 
 const mealTable = props => {
 	const tableData = props.data.map((row, index) => {
-		return <MealTableRow row={row} key={index} />;
+		return (
+			<tr>
+				<td key={index} colSpan='3'>
+					{row.food.name}
+				</td>
+				<td>{row.food.calories}</td>
+				<td>{row.food.protein}</td>
+				<td>{row.food.carbs}</td>
+				<td>{row.food.fat}</td>
+				<td>{row.food.fiber}</td>
+			</tr>
+		);
 	});
 
 	return (
