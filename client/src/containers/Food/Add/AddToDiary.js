87a6aa9a8d120d3b9140de828362434155b4c134
Shane Honanie <shaneh425@gmail.com>
2019-03-18 17:09:19 -0700

    Presentational Container for Adding Foods

---
+++ client/src/containers/Food/Add/AddToDiary.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+import Breakfest from './AllFoods/AllFoods';
+
+export class AddTo extends Component {
+	render() {
+		return (
+			<div className='container'>
+				<nav>
+					<div className='nav nav-tabs' id='nav-tab' role='tablist'>
+						<a
+							className='nav-item nav-link active'
+							id='nav-home-tab'
+							data-toggle='tab'
+							href='#nav-home'
+							role='tab'
+							aria-controls='nav-home'
+							aria-selected='true'
+						>
+							All Foods
+						</a>
+						<a
+							className='nav-item nav-link'
+							id='nav-profile-tab'
+							data-toggle='tab'
+							href='#nav-profile'
+							role='tab'
+							aria-controls='nav-profile'
+							aria-selected='false'
+						>
+							Recent
+						</a>
+						<a
+							className='nav-item nav-link'
+							id='nav-contact-tab'
+							data-toggle='tab'
+							href='#nav-contact'
+							role='tab'
+							aria-controls='nav-contact'
+							aria-selected='false'
+						>
+							Meals
+						</a>
+					</div>
+				</nav>
+				<div className='tab-content' id='nav-tabContent'>
+					<div
+						className='tab-pane fade show active'
+						id='nav-home'
+						role='tabpanel'
+						aria-labelledby='nav-home-tab'
+					>
+						<Breakfest />
+					</div>
+					<div
+						className='tab-pane fade'
+						id='nav-profile'
+						role='tabpanel'
+						aria-labelledby='nav-profile-tab'
+					>
+						test 2
+					</div>
+					<div
+						className='tab-pane fade'
+						id='nav-contact'
+						role='tabpanel'
+						aria-labelledby='nav-contact-tab'
+					>
+						test 3
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default AddTo;
