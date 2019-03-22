abf0fd9544690fff25e29b1733b7af711064811f
Shane Honanie <shaneh425@gmail.com>
2019-03-22 14:54:13 -0700

    quick calories validation

---
+++ validation/quickAdd.js
@@ -0,0 +1,69 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateQuickAddInput(data) {
+	let errors = {};
+
+	data.date = !isEmpty(data.date) ? data.date : '';
+	data.mealOfDay = !isEmpty(data.mealOfDay) ? data.mealOfDay : '';
+	data.calories = !isEmpty(data.calories) ? data.calories : '';
+	data.fat = !isEmpty(data.fat) ? data.fat : '';
+	data.protein = !isEmpty(data.protein) ? data.protein : '';
+	data.carbs = !isEmpty(data.carbs) ? data.carbs : '';
+	data.fiber = !isEmpty(data.fiber) ? data.fiber : '';
+
+	// console.log('data', data);
+
+	if (Validator.isEmpty(data.date)) {
+		errors.date = 'Date is required';
+	}
+
+	if (Validator.isEmpty(data.mealOfDay)) {
+		errors.mealOfDay = 'Meal of day is required';
+	}
+
+	// if (!Validator.isInt(data.calories)) {
+	// 	errors.calories = 'Calories must be a number';
+	// }
+
+	if (Validator.isEmpty(data.calories)) {
+		errors.calories = 'Calories is required';
+	}
+
+	// if (!Validator.isInt(data.fat)) {
+	// 	errors.fat = 'Fat must be a number';
+	// }
+
+	if (Validator.isEmpty(data.fat)) {
+		errors.fat = 'Fat is required';
+	}
+
+	// if (!Validator.isInt(data.protein)) {
+	// 	errors.protein = 'Protein must be a number';
+	// }
+
+	if (Validator.isEmpty(data.protein)) {
+		errors.protein = 'Protein is required';
+	}
+
+	// if (!Validator.isInt(data.carbs)) {
+	// 	errors.carbs = 'Carbs must be a number';
+	// }
+
+	if (Validator.isEmpty(data.carbs)) {
+		errors.carbs = 'Carbs is required';
+	}
+
+	// if (!Validator.isInt(data.fiber)) {
+	// 	errors.fiber = 'Fiber must be a number';
+	// }
+
+	if (Validator.isEmpty(data.fiber)) {
+		errors.fiber = 'Fiber is required';
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
