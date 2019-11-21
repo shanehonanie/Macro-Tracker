c14792533aeba5c8e9877b2e62c0b41cf2d2a2f9
Shane Honanie <shaneh425@gmail.com>
2019-11-20 22:44:52 -0800

    Added Collapse Classes to Navbar

--- client/src/components/Layout/NavigationItem/NavigationItem.js
+++ client/src/components/Layout/NavigationItem/NavigationItem.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 const navigationItem = props => {
 	return (
 		<ul className={`navbar-nav ${props.margin}auto`}>
-			<li className='nav-item'>
+			<li
+				className='nav-item'
+				data-toggle='collapse'
+				data-target='.navbar-collapse.show'
+			>
 				<Link className='nav-link' to={props.to}>
 					{props.name}
 				</Link>
